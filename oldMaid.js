83cba7bf262cd28844be14eeecb2280186feda4f
s0901527 <81180539+s0901527@users.noreply.github.com>
2022-02-15 07:31:32 -0600

    Update oldMaid.js

--- oldMaid.js
+++ oldMaid.js
@@ -1,24 +1,6 @@
-function hehe(){
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-  
-    // While there remain elements to shuffle...
-    while (currentIndex != 0) {
-  
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-  
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-  
-    return array;
-  }
-  
-  document.getElementById("sart").disabled = "true";
- 
+var images = [];
+Player1 = [];
+Player2 = [];
 var dict = {
     "AH" : "https://deckofcardsapi.com/static/img/AH.png",
     "AC" : "https://deckofcardsapi.com/static/img/AC.png",
@@ -150,11 +132,34 @@ var cards = [    "AH",
 "KS" ,
 "KD"];
 
-var images = [];
+function hehe(){
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+  }
+  
+  document.getElementById("sart").disabled = "true";
+ 
+
+
+//var images = [];
  
 shuffle(cards)
-Player1 = [];
-Player2 = [];
+//Player1 = [];
+//Player2 = [];
 for(var i = 0; i<25; i++){
     Player1.push(cards[i]);
 }
@@ -170,4 +175,101 @@ for(var i = 0; i<Player1.length; i++){
     document.getElementById("board").appendChild(img);
 }
  
+}
+
+function submit(){
+    while(Player1.length!=0 && Player2.length!=0){
+    g = document.getElementById("form").value
+    document.getElementById("form").value = "";
+    myArray = g.split(",");
+    for(var i =0; i<g.length;i++){
+        h = 0
+        for(var j =0;j<Player1.length;j++){
+            if(Player1[j].includes(myArray[i])){
+                Player1.splice(j,1);
+                h++;
+                
+            }
+            if(h==2){
+                break;
+            }
+        }
+        if(h==1){
+            alert("You are the OLD Maid, go back to the homepage to Play again!")
+            document.getElementById("submit").disabled = "true";
+        }
+    }
+
+    document.getElementById("board").innerHTML="";
+
+    for(var i = 0; i<Player1.length; i++){
+        var img = document.createElement("img");
+        img.style.height = '20%';
+        img.style.width = '20%';
+        img.src = dict[Player1[i]];
+        images.push(img)
+        document.getElementById("board").appendChild(img);
+    }
+
+    index = Math.floor(Math.random()*(Player1.length+1));
+    rand = Player1[index];
+    Player1.splice(index,1);
+    Player2.push(rand);
+
+    var temp = [
+        "A",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "0",
+        "J",
+        "Q",
+        "K"
+    ]
+    
+    for(var i =0; i<temp.length;i++){
+        h=0
+        for(var j =0; j<Player2.length;j++){
+            if(Player2[j].includes(temp[i])){
+                h++;
+            }
+            if(h%2==0){
+                for(var k =0; k<Player2.length;k++){
+                    if(Player2[j].includes(temp[i])){
+                        Player2.splice(j,1)
+                    }
+                }
+            }
+        }
+    }
+
+    Nindex = Math.floor(Math.random()*(Player2.length+1));
+    Nrand = Player2[index];
+    Player2.splice(Nindex,1);
+    Player1.push(Nrand);
+
+    document.getElementById("board").innerHTML="";
+
+    for(var i = 0; i<Player1.length; i++){
+        var img = document.createElement("img");
+        img.style.height = '20%';
+        img.style.width = '20%';
+        img.src = dict[Player1[i]];
+        images.push(img)
+        document.getElementById("board").appendChild(img);
+    }
+    }
+    if(Player2.length==0){
+        alert("You are the OLD Maid, go back to the homepage to Play again!")
+        document.getElementById("submit").disabled = "true";
+    }
+    else{
+        alert("You are NOT the OLD Maid, go back to the homepage to Play again!")
+        document.getElementById("submit").disabled = "true";
+    }
 }
